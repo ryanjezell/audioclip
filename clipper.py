d1940384c23aeefa648aeb1081aa7ef3856ff930
ryanjezell <ryanjezell@gmail.com>
2023-03-04 01:47:09 -0500

    added os functionality

--- clipper.py
+++ clipper.py
@@ -1,9 +1,15 @@
 import sys
 from pydub import AudioSegment
+import os
+
+current_dir = os.getcwd()
+output_dir = os.path.join(current_dir, 'output')
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 
 # Take every command line argument from index 1 onwards
 args = sys.argv[1:] 
-
 
 ## Formula = x * 1000, where x is desired seconds to clip off the end and * 1000 is accounting for millisecond format ##
 back_clip_time = 2 * 1000 
