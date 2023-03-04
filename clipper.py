69d9a97cea42806caef3313e3fb58f6342c53d13
ryanjezell <ryanjezell@gmail.com>
2023-03-04 02:46:01 -0500

    Improved Operating System Compatibility, extra portable

--- clipper.py
+++ clipper.py
@@ -9,12 +9,14 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 
-# Take every command line argument from index 1 onwards
+## Take every command line argument from index 1 onwards
 args = sys.argv[1:] 
 
 ## Formula = x * 1000, where x is desired seconds to clip off the end and * 1000 is accounting for millisecond format ##
 back_clip_time = 2 * 1000 
 
+## Export path determination
+export_path =  "output/clipped_" if os.name == 'nt' else "output\clipped_"
 
 def main():
     validCheck(args)
@@ -32,10 +34,8 @@ def backClip(*args):
     for i in lst:
         whole_video = AudioSegment.from_mp3(i)
         desired_clip = whole_video[:len(whole_video) - back_clip_time]
-        if os.name == 'nt':
-            desired_clip.export("output/clipped_" + i, format="mp3")
-        else:
-            desired_clip.export("output\clipped_" + i, format="mp3")
+        desired_clip.export(export_path + i, format="mp3")
+       
 
 
 
